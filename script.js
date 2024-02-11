450ba978c1f0d5ac2effdd87f521c374616b433f
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-11 20:55:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const p = document.getElementById('timer');
+function setTime(){
+	let now = new Date();
+	let month = now.getMonth() + 1;
+	let year = now.getFullYear();
+	let day = now.getDate();
+	let time =now.toLocaleTimeString();
+	let date = `${month}/${day}/${year}`;
+	if(Number(time.slice(0,2)) > 11){
+		time = Number(time.slice(0,2)) - 12 + time.slice(2);
+		p.innerText = `${date}, ${time} PM`;
+	}
+	//else{
+	// 	p.innerText = `${date}, ${time} AM`;
+	// }
+}
+setInterval(setTime , 1000);
+setTime();
+
